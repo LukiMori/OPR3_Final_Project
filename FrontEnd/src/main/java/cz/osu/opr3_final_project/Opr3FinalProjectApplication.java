47a1e25e926ef61585d9eecd56f259ca059d59c5
lukasmorisak <lukasmorisak@seznam.cz>
2025-10-17 22:31:00 +0200

    Initial commit

---
+++ FrontEnd/src/main/java/cz/osu/opr3_final_project/Opr3FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.opr3_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opr3FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Opr3FinalProjectApplication.class, args);
+    }
+
+}
---
+++ FrontEnd/src/test/java/cz/osu/opr3_final_project/Opr3FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.opr3_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Opr3FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
