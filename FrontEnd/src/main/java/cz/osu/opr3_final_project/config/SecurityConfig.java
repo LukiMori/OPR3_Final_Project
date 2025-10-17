8caae3355746c4081a140a5bcd2f23994decace6
lukasmorisak <lukasmorisak@seznam.cz>
2025-10-18 00:52:52 +0200

    Created securityConfig and added spring-boot-starter-security to dependencies

---
+++ FrontEnd/src/main/java/cz/osu/opr3_final_project/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package cz.osu.opr3_final_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
