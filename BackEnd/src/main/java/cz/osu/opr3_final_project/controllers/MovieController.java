3a5cc3745ff4e1da2772c2d94a286047512a8567
lukasmorisak@seznam.cz <lukasmorisak@seznam.cz>
2025-11-22 14:05:20 +0100

    Removing unnecessary movie functions from MovieController

--- BackEnd/src/main/java/cz/osu/opr3_final_project/controllers/MovieController.java
+++ BackEnd/src/main/java/cz/osu/opr3_final_project/controllers/MovieController.java
@@ -4,7 +4,6 @@ import cz.osu.opr3_final_project.model.entities.Movie;
 import cz.osu.opr3_final_project.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class MovieController {
@@ -14,18 +13,8 @@ public class MovieController {
         this.movieRepository = movieRepository;
     }
 
-    @GetMapping("/movies")
-    public List<Movie> getMovies() {
-        return movieRepository.findAll();
-    }
-
-    @GetMapping("/movies/{ID}")
+    @GetMapping("/movie/{ID}")
     public Movie getMovieById(@PathVariable Long ID) {
         return movieRepository.findById(ID).orElse(null);
-    }
-
-    @PostMapping
-    public Movie addMovie(@RequestBody Movie movie) {
-        return movieRepository.save(movie);
     }
 }
